5b467280cef9539a060f161be89c564d5d7d6cd6
Your Name <yanushevskaya.lada@mail.ru>
2020-11-29 16:16:19 +0300

    form

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ closed.addEventListener('click', (e)=>{
   modal.classList.remove('fullscreen-menu_active');
 });
 
-///////////////////
+///////////////////SLIDER
 
 const leftBtn = document.querySelector(".slider__btn--prev");
 const rightBtn = document.querySelector(".slider__btn--next");
@@ -69,6 +69,135 @@ items.style.right=`${currentRight-currentWidth}px`;
   }
 
 });
+///////SLIDESHOW
+const findBlockByAlias=(alias)=>{
+ return $(".reviews__item").filter((ndx,item)=>{
+    return $(item).attr("data-linked-with")==alias;
+   
+   
+  })
+}
+$(".interactive-avatar__link").on('click', function(e){
+  e.preventDefault();
+  const $this=$(e.currentTarget);
+  const target=$this.attr("date-open");
+  const itemToShow=findBlockByAlias(target);
+  const curItem=$this.closest(".reviews__switcher-item");
+
+  itemToShow.addClass("active").siblings().removeClass("active");
+  curItem.addClass("interactive-avatar--active").siblings().removeClass("interactive-avatar--active");
+})
+
+////////ACCORDION
+const openItem=(item)=>{
+const container=item.closest((".team__item"));
+const contentBlock=container.find(".team__content");
+const textBlock=contentBlock.find(".team__content-block");
+const reqHeight=textBlock.height();
+container.addClass("active");
+contentBlock.height(reqHeight);
+}
+
+const closeEveryItem=(container)=>{
+  const items=container.find('.team__content');
+  const itemContainer=container.find(".team__item");
+
+  console.log(itemContainer);
+  itemContainer.removeClass("active");
+  items.height(0);
+
+}
+$('.team__title').click(e=>{
+  const $this=$(e.currentTarget);
+  const container=$this.closest('.team');
+
+  const elemContainer=$this.closest('.team__item')
+
+  if(elemContainer.hasClass("active")){
+    closeEveryItem(container);
+  }else{
+    closeEveryItem(container);
+    openItem($this);
+  }
+})
+
+//MODAL
+
+const validateFields=(form,fieldsArray)=>{
+
+  fieldsArray.forEach(field=>{
+    field.removeClass("input-error")
+  if(field.val().trim()===''){
+    field.addClass('input-error');//trim обрезает пробелы
+  }
+  
+  });
+  
+  const errorFields=form.find(".input-error");
+  return errorFields.length==0;
+
+}
+
+$('.form').submit(e=>{
+  e.preventDefault();
+
+  const form=$(e.currentTarget);
+  const name=form.find("[name='name']");
+  const phone=form.find("[name='phone']");
+  const comment=form.find("[name='comment']");
+  const to=form.find("[name='to']");
+
+  const modal=$("#modal");
+  const content=modal.find(".modal__content");
+
+  modal.removeClass("error-modal");
+
+  const isValid=validateFields(form,[name,phone,comment,to]);
 
 
+if(isValid){
+  $.ajax({
+
+    url:"https://webdev-api.loftschool.com/sendmail",
+    method:"POST",
+    data:{
+      name:name.val(),
+      phone:phone.val(),
+      comment:comment.val(),
+      to:to.val()
+
+    },
+
+    success:(data)=>{
+
+      content.text(data.message)
+      console.log(data);
+      
+  $.fancybox.open({
+    src:'#modal',
+    type:"inline"
+  })
+    },
+    error:(data)=>{
+      const message=data.responseJSON.message;
+      content.text(message);
+      console.log(data);
+      modal.addClass('error-modal');
+
+      $.fancybox.open({
+        src:'#modal',
+        type:"inline"
+      })
+    }
+  });
+}
+
+  
+
+  
+})
+$(".app-submit-btn").click(e=>{
+  e.preventDefault();
+  $.fancybox.close();
+})
 
